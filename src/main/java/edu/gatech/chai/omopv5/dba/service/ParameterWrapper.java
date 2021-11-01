88d3e87e2f2722af336e39f32911732e8fda8719
Myung Choi <myungchoi@gmail.com>
2021-11-01 18:44:51 -0400

    fixed or parameters are grouped in ()

--- src/main/java/edu/gatech/chai/omopv5/dba/service/ParameterWrapper.java
+++ src/main/java/edu/gatech/chai/omopv5/dba/service/ParameterWrapper.java
@@ -279,6 +279,7 @@ public class ParameterWrapper {
 //
 		// paramList has FHIR parameters mapped Omop parameters (or columns).
 		int i = 0;
+		String upperRelationshipOrClauses = "";
 		for (ParameterWrapper param : paramList) {
 			String subWhere = "";
 			i++;
@@ -518,17 +519,35 @@ public class ParameterWrapper {
 
 			if (where != null && !where.isEmpty()) {
 				if (param.getUpperRelationship() != null && param.getUpperRelationship().equalsIgnoreCase("or")) {
-					where = where + " or " + subWhere;
+					if (upperRelationshipOrClauses.isEmpty()) {
+						upperRelationshipOrClauses = "(" + subWhere;
+					} else {
+						upperRelationshipOrClauses += " or " + subWhere;
+					}
+					// where = where + " or " + subWhere;
 				} else {
+					if (!upperRelationshipOrClauses.isEmpty()) {
+						where = where + upperRelationshipOrClauses + ")";
+						upperRelationshipOrClauses = "";
+					}
 					where = where + " and " + subWhere;
 				}
 			} else {
 				if (param.getUpperRelationship() != null && param.getUpperRelationship().equalsIgnoreCase("or")) {
-					where = subWhere;
+					if (upperRelationshipOrClauses.isEmpty()) {
+						upperRelationshipOrClauses = "(" + subWhere;
+					} else {
+						upperRelationshipOrClauses += " or " + subWhere;
+					}
+					// where = subWhere;
 				} else {
 					where = subWhere;
 				}
 			}
+		}
+
+		if (!"".equals(upperRelationshipOrClauses)) {
+			where = where + " and " + upperRelationshipOrClauses + ")";
 		}
 
 		if (where == null || where.isEmpty()) {

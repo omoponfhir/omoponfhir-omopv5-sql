25d43d97b42f69c82efdaafc8469a0af48cae42c
Myung Choi <myungchoi@gmail.com>
2023-06-21 09:41:13 -0400

    fPerson birthdate handling updated

--- src/main/java/edu/gatech/chai/omopv5/dba/service/FPersonService.java
+++ src/main/java/edu/gatech/chai/omopv5/dba/service/FPersonService.java
@@ -26,6 +26,7 @@ import java.util.List;
 
 import com.google.cloud.bigquery.FieldValueList;
 
+import edu.gatech.chai.omopv5.dba.util.SqlUtil;
 import edu.gatech.chai.omopv5.model.entity.CareSite;
 import edu.gatech.chai.omopv5.model.entity.Concept;
 import edu.gatech.chai.omopv5.model.entity.FPerson;
@@ -211,15 +212,9 @@ public interface FPersonService extends IService<FPerson> {
 				fPerson.setDayOfBirth((int) rowResult.get(columnInfo).getLongValue());
 			} else if (columnInfo.equalsIgnoreCase("person_birth_datetime")) {
 				String dateString = rowResult.get(columnInfo).getStringValue();
-				if (dateString != null && !dateString.isEmpty()) {
-					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-					Date date;
-					try {
-						date = dateFormat.parse(dateString);
-						fPerson.setBirthDateTime(date);
-					} catch (ParseException e) {
-						e.printStackTrace();
-					}
+				Date date = SqlUtil.string2DateTime(dateString);
+				if (date != null) {
+					fPerson.setBirthDateTime(date);
 				}
 			} else if (columnInfo.equalsIgnoreCase("raceConcept_concept_id")) {
 				Concept raceConcept = ConceptService._construct(rowResult, null, "raceConcept", columns);
